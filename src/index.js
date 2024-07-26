d14ed436677b8baf1ea7ae0b04bf7076acc75c95
Supertiger <saawalkhn@googlemail.com>
2024-07-26 13:24:32 +0100

    Fix cors

--- src/index.js
+++ src/index.js
@@ -1,26 +1,32 @@
-import express from 'express';
-import config from './config.js';
-import { createFolders } from './createFolders.js';
-import { uploadRouter } from './routes/uploadImagesRoutes.js';
-import { getImagesRouter } from './routes/getImagesRoutes.js';
-import { deleteImagesRouter } from './routes/deleteImagesRoutes.js';
-import { proxyRouter } from './routes/proxyRoutes.js';
-import helmet from 'helmet';
+import express from "express";
+import config from "./config.js";
+import { createFolders } from "./createFolders.js";
+import { uploadRouter } from "./routes/uploadImagesRoutes.js";
+import { getImagesRouter } from "./routes/getImagesRoutes.js";
+import { deleteImagesRouter } from "./routes/deleteImagesRoutes.js";
+import { proxyRouter } from "./routes/proxyRoutes.js";
+import helmet from "helmet";
 createFolders();
 
 const app = express();
 
-app.use(helmet({
-  crossOriginResourcePolicy: false,
-}));
-app.use(proxyRouter)
-app.use(uploadRouter)
-app.use(getImagesRouter)
-app.use(deleteImagesRouter)
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false,
+  })
+);
 
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
 
-
+app.use(proxyRouter);
+app.use(uploadRouter);
+app.use(getImagesRouter);
+app.use(deleteImagesRouter);
 
 app.listen(config.PORT, () => {
   console.log(`Nerimity CDN started at port ${config.PORT}`);
-})
+});
