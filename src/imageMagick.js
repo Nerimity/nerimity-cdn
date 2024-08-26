8cc8f53cbcc7561a4e677b1db533bd372b06b00f
Supertiger <saawalkhn@googlemail.com>
2024-08-26 20:41:51 +0100

    add removeFile function to imagemagic.js

--- src/imageMagick.js
+++ src/imageMagick.js
@@ -1,13 +1,10 @@
-import gm from 'gm';
-import path from 'path';
-import { getMetadata } from './sharp.js';
-import { Errors } from './Errors.js';
-import fs from 'fs';
+import gm from "gm";
+import path from "path";
+import { getMetadata } from "./sharp.js";
+import { Errors } from "./Errors.js";
+import fs from "fs";
 
 export const imageMagick = gm.subClass({ imageMagick: "7+" });
-
-
-
 
 /**
  * Compresses an image.
@@ -23,30 +20,27 @@ export const imageMagick = gm.subClass({ imageMagick: "7+" });
  */
 export const compressImage = async (opts) => {
   const oldMetadata = await getMetadata(opts.tempPath);
-  if (!oldMetadata) return [null, Errors.COMPRESS_ERROR()]
+  if (!oldMetadata) return [null, Errors.COMPRESS_ERROR()];
 
   const isAnimated = !!oldMetadata.pages;
 
   const parsedFilename = path.parse(opts.filename);
   const newFilename = parsedFilename.name + (isAnimated ? ".gif" : ".webp");
 
-
-  await fs.promises.mkdir(opts.newPath, {recursive: true});
-
+  await fs.promises.mkdir(opts.newPath, { recursive: true });
 
   const newPath = path.join(opts.newPath, newFilename);
 
   let im = imageMagick(opts.tempPath);
 
-  im = im.quality(90)
-    .autoOrient()
-    .coalesce();
-
-
+  im = im.quality(90).autoOrient().coalesce();
 
   if (!opts.crop) {
-    im = im
-      .resize(opts.size[0], opts.size[1], opts.size[2] === "fit" ? ">" : "^")
+    im = im.resize(
+      opts.size[0],
+      opts.size[1],
+      opts.size[2] === "fit" ? ">" : "^"
+    );
   }
 
   if (opts.crop?.length <= 2) {
@@ -54,63 +48,61 @@ export const compressImage = async (opts) => {
       .resize(opts.size[0], opts.size[1], opts.size[2] === "fit" ? ">" : "^")
       .gravity("Center")
       .crop(...opts.crop)
-      .repage("+")
+      .repage("+");
   }
 
   if (opts.crop?.length > 2) {
     im = im
       .crop(...opts.crop)
       .resize(opts.size[0], opts.size[1], opts.size[2] === "fit" ? ">" : "^")
-      .repage("+")
-  } 
-
+      .repage("+");
+  }
 
   return new Promise((resolve) => {
     im.write(newPath, async (err) => {
-      if (err) return resolve([null, Errors.COMPRESS_ERROR()])
+      if (err) return resolve([null, Errors.COMPRESS_ERROR()]);
       const newMetadata = await getMetadata(newPath);
-      if (!newMetadata) { removeFile(newPath); return resolve([null, Errors.COMPRESS_ERROR()]) }
-      resolve([{
-        path: path.join(newPath, newFilename),
-        newFilename,
-        dimensions: {width: newMetadata.width, height: newMetadata.height},
-        gif: isAnimated
-      }, null])
+      if (!newMetadata) {
+        removeFile(newPath);
+        return resolve([null, Errors.COMPRESS_ERROR()]);
+      }
+      resolve([
+        {
+          path: path.join(newPath, newFilename),
+          newFilename,
+          dimensions: { width: newMetadata.width, height: newMetadata.height },
+          gif: isAnimated,
+        },
+        null,
+      ]);
     });
-  })
-   
-
-}
-
+  });
+};
 
 /**
  *
  * @param {string | import("stream").Readable} pathOrStream
- * @param {{size?: number | [number, number], static: boolean} | undefined} opts 
+ * @param {{size?: number | [number, number], static: boolean} | undefined} opts
  * @return {Promise<[import("stream").Readable, null] | [null, Error]>} - A promise that resolves to an array containing the WebP stream and any error that occurred.
  */
 export async function miniConvert(pathOrStream, opts) {
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     let instance = imageMagick(pathOrStream);
     if (opts.static) instance = instance.selectFrame(0);
     if (opts.size) {
       if (typeof opts.size === "number") {
-        instance = instance.resize(opts.size, opts.size, ">")
-      }
-      else {
-        instance = instance.resize(opts.size[0], opts.size[1], ">")
+        instance = instance.resize(opts.size, opts.size, ">");
+      } else {
+        instance = instance.resize(opts.size[0], opts.size[1], ">");
       }
     }
 
     instance.stream("webp", (err, stream) => {
-      if (err) return resolve([null, err])
-      resolve([stream, null])
+      if (err) return resolve([null, err]);
+      resolve([stream, null]);
     });
-  })
+  });
 }
-
-
-
 
 /**
  * Converts an array of points into dimensions.
@@ -125,23 +117,32 @@ export const pointsToDimensions = (pointsStr) => {
   try {
     parsedPoints = JSON.parse(pointsStr || null);
     if (parsedPoints !== null) {
-      if (!Array.isArray(parsedPoints)) return [null, null, Errors.INVALID_POINTS()]
-      if (parsedPoints.length !== 4) return [null, null, Errors.INVALID_POINTS()]
-      const invalidPoint = parsedPoints.find(point => typeof point !== "number" || isNaN(point) || point < 0 || point > 9999);
-      if (invalidPoint) return [null, null, Errors.INVALID_POINTS()]
+      if (!Array.isArray(parsedPoints))
+        return [null, null, Errors.INVALID_POINTS()];
+      if (parsedPoints.length !== 4)
+        return [null, null, Errors.INVALID_POINTS()];
+      const invalidPoint = parsedPoints.find(
+        (point) =>
+          typeof point !== "number" || isNaN(point) || point < 0 || point > 9999
+      );
+      if (invalidPoint) return [null, null, Errors.INVALID_POINTS()];
       dimensions = parsedPoints && getDimensions(parsedPoints);
-      return [dimensions, parsedPoints, null]
+      return [dimensions, parsedPoints, null];
     }
-    return [null, null, null]
-  } catch(err) {
-    return [null, null, Errors.INVALID_POINTS()]
+    return [null, null, null];
+  } catch (err) {
+    return [null, null, Errors.INVALID_POINTS()];
   }
-}
-
+};
 
 function getDimensions(points) {
-  const [startX, startY, endX, endY ] = points;
+  const [startX, startY, endX, endY] = points;
   const width = Math.abs(endX - startX);
   const height = Math.abs(endY - startY);
   return { width, height };
+}
+
+export async function removeFile(path) {
+  if (!path) return;
+  return await fs.promises.unlink(path).catch(() => {});
 }
